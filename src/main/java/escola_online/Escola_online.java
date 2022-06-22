b84eec6fd49448d4a237833c752a59aee6c4daac
Diego Nunes <nunesdiegond@gmail.com>
2022-06-22 14:57:34 -0300

    começando a fazer a página da escola

--- src/main/java/escola_online/Escola_online.java
+++ src/main/java/escola_online/Escola_online.java
@@ -17,11 +17,15 @@ public class Escola_online {
 
     public static void main(String[] args) {
         
+        
+        // INICIO DE CÓDIGOS DE TESTE DE FUNCIONAMENTO
         Aluno aluno1 = new Aluno(876, "João", "29/12/1900", 122, 'M', 3, 6);
         
         Professor prof = new Professor(234, "Tião", "01/01/2020", 'F', "Bio", 45);
         
         JOptionPane.showMessageDialog(null,"Nome do aluno: "+aluno1.getNome()+"\nturma: "+aluno1.getTurma());
         JOptionPane.showMessageDialog(null,"Professor: "+prof.getNome());
+        //FIM DOS TESTES DE FUNCIONAMENTO
+        
     }
 }
